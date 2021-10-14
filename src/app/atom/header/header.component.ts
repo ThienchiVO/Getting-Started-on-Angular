4d119d8f1747fda309faea89e526e09f78a37704
ThienchiVO <thienchi.vo.perso@gmail.com>
2021-10-14 17:03:52 +0200

    1 file modified

--- src/app/atom/header/header.component.ts
+++ src/app/atom/header/header.component.ts
@@ -6,40 +6,40 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  @Input() headerText: string = "";
+  @Input() headerText: any = '';
   @Input() headerSize: number = 1;
-  headerHtml: string = "";
-  
+  headerHtml: string = '';
+
   constructor() {}
 
   ngOnInit() {
     switch (this.headerSize) {
       case 1: {
-        this.headerHtml = "<h1>" + this.headerText + "</h1>";
+        this.headerHtml = '<h1>' + this.headerText + '</h1>';
         break;
       }
       case 2: {
-        this.headerHtml = "<h2>" + this.headerText + "</h2>";
+        this.headerHtml = '<h2>' + this.headerText + '</h2>';
         break;
       }
       case 3: {
-        this.headerHtml = "<h3>" + this.headerText + "</h3>";
+        this.headerHtml = '<h3>' + this.headerText + '</h3>';
         break;
       }
       case 4: {
-        this.headerHtml = "<h4>" + this.headerText + "</h4>";
+        this.headerHtml = '<h4>' + this.headerText + '</h4>';
         break;
       }
       case 5: {
-        this.headerHtml = "<h5>" + this.headerText + "</h5>";
+        this.headerHtml = '<h5>' + this.headerText + '</h5>';
         break;
       }
       case 6: {
-        this.headerHtml = "<h6>" + this.headerText + "</h6>";
+        this.headerHtml = '<h6>' + this.headerText + '</h6>';
         break;
       }
       default: {
-        this.headerHtml = "La taille du titre doit être comprise entre 1 et 6";
+        this.headerHtml = 'La taille du titre doit être comprise entre 1 et 6';
       }
     }
   }
