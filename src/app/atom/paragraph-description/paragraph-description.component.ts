fb61b85a73958c4adf61f74da35c610f6290d277
ThienchiVO <thienchi.vo.perso@gmail.com>
2021-10-14 14:31:10 +0200

    2 files modified, 1 added

--- src/app/atom/paragraph-description/paragraph-description.component.ts
+++ src/app/atom/paragraph-description/paragraph-description.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-paragraph-description',
   templateUrl: './paragraph-description.component.html',
-  styleUrls: ['./paragraph-description.component.css']
+  styleUrls: ['./paragraph-description.component.css'],
 })
 export class ParagraphDescriptionComponent implements OnInit {
+  @Input() textDescription: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
