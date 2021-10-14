adf6d90c9aa44741db6c1802227c6f2ac33a02a4
ThienchiVO <thienchi.vo.perso@gmail.com>
2021-10-14 12:06:04 +0200

    1 file modified, 3 added

---
+++ src/app/atom/product-link/product-link.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-link',
+  templateUrl: './product-link.component.html',
+  styleUrls: ['./product-link.component.css']
+})
+export class ProductLinkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
