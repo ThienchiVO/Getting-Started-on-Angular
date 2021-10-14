9f3046d06e8f35c6b424e70a5a95964ad617abc2
ThienchiVO <thienchi.vo.perso@gmail.com>
2021-10-14 16:10:23 +0200

    1 file modified, 1 added

--- src/app/atom/header/header.component.ts
+++ src/app/atom/header/header.component.ts
@@ -8,6 +8,37 @@ import { Component, Input, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   @Input() headerText: string = '';
   @Input() headerSize: number = 1;
+  headerHtml: string = "";
+
+  switch(headerSize){
+    case 1:{
+      this.headerHtml = "<h1>" + this.headerText + "</h1>";
+      break;
+    }
+    case 2:{
+      this.headerHtml = "<h1>" + this.headerText + "</h1>";
+      break;
+    }
+    case 3:{
+      this.headerHtml = "<h1>" + this.headerText + "</h1>";
+      break;
+    }
+    case 4:{
+      this.headerHtml = "<h1>" + this.headerText + "</h1>";
+      break;
+    }
+    case 5:{
+      this.headerHtml = "<h1>" + this.headerText + "</h1>";
+      break;
+    }
+    case 6:{
+      this.headerHtml = "<h1>" + this.headerText + "</h1>";
+      break;
+    }
+    default:{
+
+    } 
+  }
   constructor() {}
 
   ngOnInit() {}
