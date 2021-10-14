3fc4c609a580271143d03dd5bdcf36e576f54d3e
ThienchiVO <thienchi.vo.perso@gmail.com>
2021-10-14 15:37:30 +0200

    4 files modified

--- src/app/molecules/fiche-produit/fiche-produit.component.ts
+++ src/app/molecules/fiche-produit/fiche-produit.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../../products';
 
 @Component({
   selector: 'app-fiche-produit',
   templateUrl: './fiche-produit.component.html',
-  styleUrls: ['./fiche-produit.component.css']
+  styleUrls: ['./fiche-produit.component.css'],
 })
 export class FicheProduitComponent implements OnInit {
+  
+  @Input() product!: Product;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  share = () => window.alert('The product has been shared!');
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
   }
-
 }
