3f5906caedd77c4cef20d1997ccdd0b2e85cd731
ThienchiVO <thienchi.vo.perso@gmail.com>
2021-10-14 18:31:38 +0200

    4 files modified, 3 deleted

--- src/app/atom/link/link.component.ts
+++ src/app/atom/link/link.component.ts
@@ -6,8 +6,8 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./link.component.css'],
 })
 export class LinkComponent implements OnInit {
-  @Input() productName: string = '';
-  @Input() productId: number = 0;
+  @Input() linkName: string = '';
+  @Input() linkId: number = 0;
   constructor() {}
 
   ngOnInit() {}
