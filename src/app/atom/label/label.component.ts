9d4baac807939ee7148c12be2d8af338e90e4f64
ThienchiVO <thienchi.vo.perso@gmail.com>
2021-10-15 18:46:57 +0200

    1 file modified, 1 added

--- src/app/atom/label/label.component.ts
+++ src/app/atom/label/label.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-label',
   templateUrl: './label.component.html',
-  styleUrls: ['./label.component.css']
+  styleUrls: ['./label.component.css'],
 })
 export class LabelComponent implements OnInit {
+  @Input() labelText: string = '';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
